96c0cfca720b575b7e6b0b55b3a410d199fa370d
unknown <ivana.unitedforce@gmail.com>
2017-09-10 07:29:48 +0200

    java1.8

--- src/main/java/BSEP/service/UserDetailsServiceImpl.java
+++ src/main/java/BSEP/service/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package BSEP.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import BSEP.beans.User;
+import BSEP.beans.UserAuthority;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -29,16 +30,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     if (user == null) {
       throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
     } else {
-    	/*List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+    	List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
     	for (UserAuthority ua: user.getUserAuthorities()) {
     		grantedAuthorities.add(new SimpleGrantedAuthority(ua.getAuthority().getName()));
-    	}*/
+    	}
     	
     	//Java 1.8 way   	
-    	List<GrantedAuthority> grantedAuthorities = user.getUserAuthorities().stream()
+    	/*List<GrantedAuthority> grantedAuthorities = user.getUserAuthorities().stream()
                 .map(authority -> new SimpleGrantedAuthority(authority.getAuthority().getName()))
                 .collect(Collectors.toList());
-    	
+    	*/
     	return new org.springframework.security.core.userdetails.User(
     		  user.getUsername(),
     		  user.getPassword(),
